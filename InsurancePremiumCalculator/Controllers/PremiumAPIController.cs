d768258947003bb45db8c49f715e314c6325b096
SivaSaiSambela <sivasai.sambela@gmail.com>
2025-11-17 20:37:06 +0530

    added PremiumApiController with calculate endpoint

---
+++ InsurancePremiumCalculator/Controllers/PremiumAPIController.cs
@@ -0,0 +1,30 @@
+﻿using InsurancePremiumCalculator.Models;
+using InsurancePremiumCalculator.Services.IServices;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InsurancePremiumCalculator.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PremiumAPIController : ControllerBase
+    {
+        private readonly IPremiumCalculatorService _service;
+
+        public PremiumAPIController(IPremiumCalculatorService service)
+        {
+            _service = service;
+        }
+
+        [HttpPost("Calculate")]
+        public IActionResult Calculate([FromBody] PremiumCalculationRequest request)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var result = _service.Calculate(request);
+
+            return Ok(result);
+        }
+    }
+}
