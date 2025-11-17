278aebc0763a80398d87684a8eeef3ada966d92a
SivaSaiSambela <sivasai.sambela@gmail.com>
2025-11-17 20:41:34 +0530

    MVC UI page added

---
+++ InsurancePremiumCalculator/Controllers/PremiumController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace InsurancePremiumCalculator.Controllers
+{
+    public class PremiumController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.Occupations = new List<string>
+            {
+                "Cleaner", "Doctor", "Author", "Farmer", "Mechanic", "Florist", "Other"
+            };
+
+            return View();
+        }
+    }
+}
